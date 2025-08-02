e973df7e2495ac3a3adacfda016c23ac03c055f8
Эмиль Ерофеевский <emilerofeevskij@gmail.com>
2025-08-02 07:03:53 +0500

    feat: added sodium and  interfaces for encryption

--- src/encrypting/interfaces/ICryptoError.h
+++ src/encrypting/interfaces/ICryptoError.h
@@ -1,4 +1,12 @@
 #ifndef ICRYPTOERROR_H
 #define ICRYPTOERROR_H
 
+#include <QString>
+
+class ICryptoError{
+public:
+    virtual ~ICryptoError() = default;
+    virtual QString message() const = 0;
+};
+
 #endif // ICRYPTOERROR_H
